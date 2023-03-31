a84fa40c3b9e71d5e91283e8cebc1591b5114456
OnishchenkoIvan <iv.onishchenko@mail.ru>
2023-03-31 19:36:34 +0300

    fix: app-reducer

--- src/features/Login/auth-reducer.ts
+++ src/features/Login/auth-reducer.ts
@@ -1,4 +1,3 @@
-import { setAppStatusAC } from "app/app-reducer";
 import { authAPI, LoginParamsType } from "api/todolists-api";
 import {
   handleServerAppError,
@@ -6,15 +5,13 @@ import {
 } from "utils/error-utils";
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { AppThunk } from "app/store";
-
-const initialState = {
-  isLoggedIn: false,
-};
-export type InitialStateType = typeof initialState;
+import { appActions } from "app/app-reducer";
 
 const slice = createSlice({
   name: "auth",
-  initialState,
+  initialState: {
+    isLoggedIn: false,
+  },
   reducers: {
     setIsLoggedIn: (state, action: PayloadAction<{ isLoggedIn: boolean }>) => {
       state.isLoggedIn = action.payload.isLoggedIn;
@@ -28,13 +25,13 @@ export const authActions = slice.actions;
 export const loginTC =
   (data: LoginParamsType): AppThunk =>
   (dispatch) => {
-    dispatch(setAppStatusAC("loading"));
+    dispatch(appActions.setAppStatus({ status: "loading" }));
     authAPI
       .login(data)
       .then((res) => {
         if (res.data.resultCode === 0) {
           dispatch(authActions.setIsLoggedIn({ isLoggedIn: true }));
-          dispatch(setAppStatusAC("succeeded"));
+          dispatch(appActions.setAppStatus({ status: "succeeded" }));
         } else {
           handleServerAppError(res.data, dispatch);
         }
@@ -44,13 +41,13 @@ export const loginTC =
       });
   };
 export const logoutTC = (): AppThunk => (dispatch) => {
-  dispatch(setAppStatusAC("loading"));
+  dispatch(appActions.setAppStatus({ status: "loading" }));
   authAPI
     .logout()
     .then((res) => {
       if (res.data.resultCode === 0) {
         dispatch(authActions.setIsLoggedIn({ isLoggedIn: false }));
-        dispatch(setAppStatusAC("succeeded"));
+        dispatch(appActions.setAppStatus({ status: "succeeded" }));
       } else {
         handleServerAppError(res.data, dispatch);
       }
