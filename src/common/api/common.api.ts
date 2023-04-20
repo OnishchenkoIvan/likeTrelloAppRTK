17f96d8d71b0631967613cc9c2a53d8024846df1
OnishchenkoIvan <iv.onishchenko@mail.ru>
2023-04-20 15:51:12 +0300

    add: Secret

--- src/common/api/common.api.ts
+++ src/common/api/common.api.ts
@@ -4,6 +4,6 @@ export const instance = axios.create({
   baseURL: "https://social-network.samuraijs.com/api/1.1/",
   withCredentials: true,
   headers: {
-    "API-KEY": "408740c5-1e6b-42b6-a275-abc2b7556ad3",
+    "API-KEY": process.env.REACT_APP_API_KEY,
   },
 });
